72d0973a8e0cfce7103c6fe2027025063a13211a
Niklas Klein <mail@taig.io>
2015-07-29 21:31:01 +0200

    Fix robotest dependency

--- project/Build.scala
+++ project/Build.scala
@@ -90,7 +90,7 @@ extends	android.AutoBuild
 		.settings(
 			fork in Test := true,
 			libraryDependencies ++= (
-				"com.geteit" %% "robotest" % "0.13" % "test" ::
+				"com.geteit" %% "robotest" % "0.12" % "test" ::
 				"org.scalatest" %% "scalatest" % "2.2.5" % "test" ::
 				Nil
 			),
