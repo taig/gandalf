5bb51d498190d537404f40067ac80abec02fdd4e
Niklas Klein <mail@taig.io>
2015-09-06 23:59:43 +0200

    Consistent naming

--- android/src/main/scala/io/taig/android/bsts/Data.scala
+++ android/src/main/scala/io/taig/android/bsts/Data.scala
@@ -14,23 +14,23 @@ trait Data[-V <: View, T] {
 }
 
 object Data {
-    implicit val `Widget[Spinner, Int]` = new Data[Spinner, Int] {
+    implicit val `Data[Spinner, Int]` = new Data[Spinner, Int] {
         override def data( view: Spinner ) = view.getSelectedItemPosition
 
         override def data( view: Spinner, value: Int ) = view.setSelection( value )
     }
 
-    implicit val `Widget[TextView, String]` = new Data[TextView, String] {
+    implicit val `Data[TextView, String]` = new Data[TextView, String] {
         override def data( view: TextView ) = view.getText.toString
 
         override def data( view: TextView, value: String ) = view.setText( value )
     }
 
-    implicit val `Widget[TextInputLayout, String]` = new Data[TextInputLayout, String] {
-        override def data( view: TextInputLayout ) = `Widget[TextView, String]`.data( view.getEditText )
+    implicit val `Data[TextInputLayout, String]` = new Data[TextInputLayout, String] {
+        override def data( view: TextInputLayout ) = implicitly[Data[TextView, String]].data( view.getEditText )
 
         override def data( view: TextInputLayout, value: String ) = {
-            `Widget[TextView, String]`.data( view.getEditText, value )
+            implicitly[Data[TextView, String]].data( view.getEditText, value )
         }
     }
 }
