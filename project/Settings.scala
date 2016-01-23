c637670ec65f6178b403ede69e5be62c3ed2da93
Niklas Klein <mail@taig.io>
2016-01-23 22:43:55 +0100

    sbt 0.13.10-RC1, for teh kicks

--- project/Settings.scala
+++ project/Settings.scala
@@ -1,13 +1,14 @@
+import sbt._
 import sbt.Keys._
 
 object Settings {
-    val common = Seq(
+    val common = Def.settings(
         organization := "io.taig",
         scalacOptions ++= (
             "-deprecation" ::
-            "-feature" ::
-            Nil
-        ),
+                "-feature" ::
+                Nil
+            ),
         scalaVersion := "2.11.7",
         version := "0.2.0"
     )
