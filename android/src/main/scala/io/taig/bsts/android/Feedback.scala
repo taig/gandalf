184fe66012a7a486c032b21578a97615a0534981
Niklas Klein <mail@taig.io>
2016-01-28 01:24:05 +0100

    Add TextInputLayout feedback instance

--- android/src/main/scala/io/taig/bsts/android/Feedback.scala
+++ android/src/main/scala/io/taig/bsts/android/Feedback.scala
@@ -1,21 +1,35 @@
 package io.taig.bsts.android
 
+import android.support.design.widget.TextInputLayout
 import android.view.View
 import android.widget.TextView
 
-trait Feedback[V <: View] {
-    def set( view: V, value: Option[String] ): Unit
+trait Feedback[-V <: View] {
+    def set( view: V, value: Option[CharSequence] ): Unit
 
     def get( view: V ): Option[String]
 }
 
 object Feedback {
-    implicit val `Feedback[TextView]`: Feedback[TextView] = new Feedback[TextView] {
-        override def set( view: TextView, value: Option[String] ): Unit = view.setError( value.orNull )
+    def instance[V <: View]( s: ( V, Option[CharSequence] ) ⇒ Unit, g: V ⇒ Option[String] ): Feedback[V] = new Feedback[V] {
+        override def set( view: V, value: Option[CharSequence] ): Unit = s( view, value )
 
-        override def get( view: TextView ): Option[String] = view.getError match {
+        override def get( view: V ): Option[String] = g( view )
+    }
+
+    implicit val `Feedback[TextView]`: Feedback[TextView] = instance(
+        ( view, error ) ⇒ view.setError( error.orNull ),
+        _.getError match {
             case error if error.length() == 0 ⇒ None
             case error                        ⇒ Some( error.toString )
         }
-    }
+    )
+
+    implicit val `Feedback[TextInputLayout]`: Feedback[TextInputLayout] = instance(
+        ( view, error ) ⇒ view.setError( error.orNull ),
+        _.getError match {
+            case error if error.length() == 0 ⇒ None
+            case error                        ⇒ Some( error.toString )
+        }
+    )
 }
--- android/src/main/scala/io/taig/bsts/android/ops/feedback.scala
+++ android/src/main/scala/io/taig/bsts/android/ops/feedback.scala
@@ -6,7 +6,7 @@ import io.taig.bsts.android.{ Feedback, Extraction, Injection }
 final class feedback[V <: View]( view: V ) {
     def feedback( implicit f: Feedback[V] ): Option[String] = f.get( view )
 
-    def feedback_=( error: String )( implicit f: Feedback[V] ): Unit = f.set( view, Option( error ) )
+    def feedback_=( error: CharSequence )( implicit f: Feedback[V] ): Unit = f.set( view, Option( error ) )
 
     def clear( implicit f: Feedback[V] ): Unit = f.set( view, None )
 }
