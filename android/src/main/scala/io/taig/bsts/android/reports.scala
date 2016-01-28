95dcd69cc9888ee4334c122aabafb8d734c0d47c
Niklas Klein <mail@taig.io>
2016-01-28 03:05:58 +0100

    Add missing report instances

--- android/src/main/scala/io/taig/bsts/android/reports.scala
+++ android/src/main/scala/io/taig/bsts/android/reports.scala
@@ -1,14 +1,41 @@
 package io.taig.bsts.android
 
 import android.content.Context
+import android.support.annotation.StringRes
+import io.taig.bsts.{ Error, Rule }
 import io.taig.bsts.android.resources.R
 import io.taig.bsts.report.Report
 import io.taig.bsts.rules.all._
+import shapeless.HList
+import shapeless.record._
 
 import scala.language.implicitConversions
 
 object reports {
-    implicit def `Report[required]`( implicit context: Context ) = {
-        Report( required )( context.getString( R.string.bsts_required ) )
+    private def report[I <: String, A <: HList]( rule: Rule[I, _, A], @StringRes message: Int )(
+        implicit
+        context: Context
+    ): Report.Aux[Error[I, A], String] = Report( rule )( context.getString( message ) )
+
+    implicit def `Report[email]`( implicit context: Context ) = report( email, R.string.bsts_email )
+
+    implicit def `Report[exactly]`( implicit context: Context ) = Report( exactly _ ) as { arguments ⇒
+        context.getString( R.string.bsts_exactly ).format( arguments( "expected" ) )
     }
+
+    implicit def `Report[matches]`( implicit context: Context ) = Report( matches _ ) as { _ ⇒
+        context.getString( R.string.bsts_matches )
+    }
+
+    implicit def `Report[max]`( implicit context: Context ) = Report( max _ ) as { arguments ⇒
+        context.getString( R.string.bsts_max ).format( arguments( "expected" ) )
+    }
+
+    implicit def `Report[min]`( implicit context: Context ) = Report( min _ ) as { arguments ⇒
+        context.getString( R.string.bsts_min ).format( arguments( "expected" ) )
+    }
+
+    implicit def `Report[phone]`( implicit context: Context ) = report( phone, R.string.bsts_phone )
+
+    implicit def `Report[required]`( implicit context: Context ) = report( required, R.string.bsts_required )
 }
