330643cf7a24279d417b3e10202ed19c77ca0073
Niklas Klein <mail@taig.io>
2016-02-16 19:29:14 +0100

    Add noSpaces transformation

--- transformation/src/main/scala/io/taig/bsts/transformation/string.scala
+++ transformation/src/main/scala/io/taig/bsts/transformation/string.scala
@@ -2,6 +2,8 @@ package io.taig.bsts.transformation
 
 trait string {
     val trim = Transformation[String, String]( "trim" )( _.trim )
+    
+    val noSpaces = Transformation[String, String]( "noSpaces" )( _.replaceAll( "\\s", "" ) )
 }
 
 object string extends string
