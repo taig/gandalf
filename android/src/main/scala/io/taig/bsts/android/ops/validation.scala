26be053d5ac6b58a8de09f1c428cfbdf5f309671
Niklas Klein <mail@taig.io>
2016-02-22 17:42:57 +0100

    Use viewvalue type aliases

--- android/src/main/scala/io/taig/bsts/android/ops/validation.scala
+++ android/src/main/scala/io/taig/bsts/android/ops/validation.scala
@@ -1,11 +1,11 @@
 package io.taig.bsts.android.ops
 
 import android.view.View
-import cats.data.{ NonEmptyList, OneAnd, Validated }
 import cats.data.Validated.{ Invalid, Valid }
-import io.taig.android.viewvalue.{ Attribute, Extraction, Injection }
-import io.taig.android.viewvalue.syntax.value._
+import cats.data.{ NonEmptyList, OneAnd, Validated }
 import io.taig.android.viewvalue.syntax.error._
+import io.taig.android.viewvalue.syntax.value._
+import io.taig.android.viewvalue.{ Extraction, Injection }
 import io.taig.bsts.Validation
 import io.taig.bsts.report.Report
 import shapeless.HList
@@ -13,17 +13,15 @@ import shapeless.HList
 class validation[I, O, V <: HList, E]( validation: Validation.Aux[I, O, V, E] ) {
     def validate[V <: View]( view: V )(
         implicit
-        v: Extraction[Attribute.Value, V, I],
-        e: Injection[Attribute.Error, V, Option[String]],
+        v: Extraction.Value[V, I],
+        e: Injection.Error[V, Option[CharSequence]],
         r: Report.Aux[Validated[E, O], Validated[NonEmptyList[String], O]]
-    ): Validated[NonEmptyList[String], O] = {
-        r.report( validation.validate( view.value[I] ) ) match {
-            case valid @ Valid( _ ) ⇒
-                view.error = None
-                valid
-            case invalid @ Invalid( OneAnd( error, _ ) ) ⇒
-                view.error = Some( error )
-                invalid
-        }
+    ): Validated[NonEmptyList[String], O] = r.report( validation.validate( view.value[I] ) ) match {
+        case valid @ Valid( _ ) ⇒
+            view.error = None
+            valid
+        case invalid @ Invalid( OneAnd( error, _ ) ) ⇒
+            view.error = Some( error )
+            invalid
     }
 }
