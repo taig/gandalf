9a2ca01c85d1c677ecaafd00f4af1c4b8fc75b54
Niklas Klein <mail@taig.io>
2016-02-23 12:25:13 +0100

    Add missing ReportableError Report typeclass

--- report/src/main/scala/io/taig/bsts/report/ReportableError.scala
+++ report/src/main/scala/io/taig/bsts/report/ReportableError.scala
@@ -7,3 +7,11 @@ class ReportableError[N <: String, A <: HList, O]( val e: Error[N, A], val r: Re
     implicit
     w: Witness.Aux[N]
 ) extends Error[N, A]( e.arguments )
+
+object ReportableError {
+    implicit def reportReportableError[N <: String, A <: HList, O]: Report[ReportableError[N, A, O], O] = {
+        new Report[ReportableError[N, A, O], O] {
+            override def report( error: ReportableError[N, A, O] ) = error.r.report( error.e )
+        }
+    }
+}
