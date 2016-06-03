b102fd6c94ac1686a468b5c6063db5021d54acf3
Niklas Klein <mail@taig.io>
2016-06-03 16:31:52 -0500

    Decouple error type class from validation

--- core/src/main/scala/io/taig/gandalf/ops/validation.scala
+++ core/src/main/scala/io/taig/gandalf/ops/validation.scala
@@ -4,7 +4,7 @@ import io.taig.gandalf._
 import io.taig.gandalf.syntax.aliases._
 
 class validation[V <: Validation]( validation: V ) {
-    def validate( value: V#Input )( implicit ev: Evaluation[V], er: Error[V] ): Result[V#Output] = ev.validate( value )
+    def validate( value: V#Input )( implicit e: Evaluation[V] ): Result[V#Output] = e.validate( value )
 
     def as( message: String ): MessageValidation[V] = MessageValidation( validation, Error.instance( message ) )
 }
