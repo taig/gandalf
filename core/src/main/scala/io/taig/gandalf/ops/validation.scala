51b049acb1063950bf2a29ea550809da74e18802
Niklas Klein <mail@taig.io>
2016-06-04 20:58:29 -0500

    Test that demonstrates the Contravariance we're lacking

--- core/src/main/scala/io/taig/gandalf/ops/validation.scala
+++ core/src/main/scala/io/taig/gandalf/ops/validation.scala
@@ -5,6 +5,4 @@ import io.taig.gandalf.syntax.aliases._
 
 class validation[V <: Validation]( validation: V ) {
     def validate( value: V#Input )( implicit e: Evaluation[V] ): Result[V#Output] = e.validate( value )
-
-    def as( message: String ): MessageValidation[V] = MessageValidation( validation, Error.instance( message ) )
 }
