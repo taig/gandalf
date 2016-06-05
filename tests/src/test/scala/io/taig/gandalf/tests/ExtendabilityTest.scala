72d458cdf5b52e4d612f6c660004fa64937972e9
Niklas Klein <mail@taig.io>
2016-06-05 12:19:18 -0500

    Add extendability tests \o/

---
+++ tests/src/test/scala/io/taig/gandalf/tests/ExtendabilityTest.scala
@@ -0,0 +1,27 @@
+package io.taig.gandalf.tests
+
+import cats.data.Validated._
+import io.taig.gandalf.predef.{ Required, Trim }
+import io.taig.gandalf.syntax.all._
+
+class ExtendabilityTest extends Suite {
+    it should "be possible to create custom Actions with inheritance" in {
+        trait MyRequired extends ( Trim <~> Required )
+        object MyRequired extends MyRequired
+
+        MyRequired.validate( "foo" ) shouldBe valid( "foo" )
+        MyRequired.validate( "  foo   " ) shouldBe valid( "foo" )
+        MyRequired.validate( "" ) shouldBe invalidNel( "Required" )
+        MyRequired.validate( "    " ) shouldBe invalidNel( "Required" )
+    }
+
+    it should "be possible to create custom Actions with type aliases" in {
+        type MyRequired = Trim <~> Required
+        val MyRequired = new MyRequired
+
+        MyRequired.validate( "foo" ) shouldBe valid( "foo" )
+        MyRequired.validate( "  foo   " ) shouldBe valid( "foo" )
+        MyRequired.validate( "" ) shouldBe invalidNel( "Required" )
+        MyRequired.validate( "    " ) shouldBe invalidNel( "Required" )
+    }
+}
