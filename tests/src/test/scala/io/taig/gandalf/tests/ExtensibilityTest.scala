0c0ae56e23bed57e23f03fb39ef2c14e87751717
Niklas Klein <mail@taig.io>
2016-06-13 16:06:21 -0500

    More tests to proof the awesomeness

--- tests/src/test/scala/io/taig/gandalf/tests/ExtensibilityTest.scala
+++ tests/src/test/scala/io/taig/gandalf/tests/ExtensibilityTest.scala
@@ -5,6 +5,7 @@ import io.taig.gandalf._
 import io.taig.gandalf.predef._
 import io.taig.gandalf.predef.messages._
 import io.taig.gandalf.syntax.all._
+import shapeless.Witness
 
 class ExtensibilityTest extends Suite {
     it should "be possible to create custom Actions with inheritance" in {
@@ -34,6 +35,18 @@ class ExtensibilityTest extends Suite {
             implicit val error = Error.instance[MyRequired]( "foobar" )
         }
 
+        MyRequired.validate( Some( "foobar" ) ) shouldBe valid( "foobar" )
         MyRequired.validate( Some( "" ) ) shouldBe invalidNel( "foobar" )
+        MyRequired.validate( None ) shouldBe invalidNel( "foobar" )
+
+        trait MyMatch extends ( Trim <~> Matches[Witness.`"foo"`.T, String] )
+
+        object MyMatch extends MyMatch {
+            implicit val error = Error.instance[MyMatch]( "foobar" )
+        }
+
+        MyMatch.validate( "foo" ) shouldBe valid( "foo" )
+        MyMatch.validate( " foo  " ) shouldBe valid( "foo" )
+        MyMatch.validate( "" ) shouldBe invalidNel( "foobar" )
     }
 }
