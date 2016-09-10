e56457daaf755dc488325df176730548210e9ed5
Niklas Klein <mail@taig.io>
2016-09-10 16:17:35 +0200

    Remove toString representation for now

--- macros/src/main/scala/io/taig/gandalf/macros/Obey.scala
+++ macros/src/main/scala/io/taig/gandalf/macros/Obey.scala
@@ -2,6 +2,6 @@ package io.taig.gandalf.macros
 
 import io.taig.gandalf.core.Rule
 
-case class Obey[L, R <: Rule]( value: R#Output ) extends AnyVal with Serializable {
-    override def toString = value.toString
-}
+case class Obey[L, R <: Rule]( value: R#Output )
+    extends AnyVal
+    with Serializable
