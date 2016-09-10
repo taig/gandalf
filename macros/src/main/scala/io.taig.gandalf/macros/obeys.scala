83a1a1a14e7cb4f518646637bc20ca167b164c04
Niklas Klein <mail@taig.io>
2016-09-10 15:59:26 +0200

    Feels like feature complete, baby

--- macros/src/main/scala/io.taig.gandalf/macros/obeys.scala
+++ macros/src/main/scala/io.taig.gandalf/macros/obeys.scala
@@ -13,25 +13,39 @@ class obeys[R <: Rule]( rule: R ) extends StaticAnnotation {
 private object obeys {
     def apply( c: whitebox.Context )( annottees: c.Expr[Any]* ): c.Expr[Any] = {
         import c.universe._
-        import termNames.CONSTRUCTOR
 
         val trees = annottees.map( _.tree )
-        val Expr( ValDef( _, _, input, _ ) ) = annottees.head
+
+        val input = annottees.head.tree match {
+            case valDef: ValOrDefDef ⇒ valDef.tpt
+            case tree ⇒
+                c.abort(
+                    c.enclosingPosition,
+                    s"@obeys can not be applied to '${show( tree )}'"
+                )
+        }
 
         val target = c.prefix.tree match {
-            case q"new obeys[$validation]" ⇒
-                validation
+            case q"new obeys[$validation]" ⇒ validation
             case q"new obeys( $validation )" ⇒
                 val retyped = retype( c )( validation, input )
                 c.typecheck( q"$retyped" )
             case _ ⇒ c.abort(
                 c.enclosingPosition,
-                "Illegal @obeys format. Can bei either @obeys[trim.type ~> " +
-                    "nonEmpty.type] or @obeys( trim ~> nonEmpty )"
+                "Illegal @obeys format. Can bei either @obeys[nonEmpty.type] " +
+                    "or @obeys( trim ~> nonEmpty )"
             )
         }
 
-        def newType( lhs: Tree ) = tq"_root_.io.taig.gandalf.macros.Obey[$lhs, $target]"
+        trees match {
+            case List( tree ) ⇒ expandDef( c )( tree, target )
+            case _            ⇒ expandClass( c )( trees, target )
+        }
+    }
+
+    def expandClass( c: whitebox.Context )( trees: Seq[c.Tree], target: c.Tree ): c.Expr[Any] = {
+        import c.universe._
+        import termNames.CONSTRUCTOR
 
         val valDef = trees
             .collectFirst { case valDef: ValDef ⇒ valDef }
@@ -52,13 +66,13 @@ private object obeys {
             }
 
         val newBody = body.map {
-            case target @ ValDef( mods, name, tpt, rhs ) if valDef equalsStructure target ⇒
-                ValDef( mods, name, newType( tpt ), rhs )
+            case t @ ValDef( mods, name, tpt, rhs ) if valDef equalsStructure t ⇒
+                ValDef( mods, name, obey( c )( tpt, target ), rhs )
             case constructor @ DefDef( mods, CONSTRUCTOR, tparams, vparamss, tpt, rhs ) ⇒
                 val newVparamss = vparamss.map { vparams ⇒
                     vparams.map {
-                        case target @ ValDef( mods, name, tpt, rhs ) if valDef.name == target.name ⇒
-                            ValDef( mods, name, newType( tpt ), rhs )
+                        case t @ ValDef( mods, name, tpt, rhs ) if valDef.name == t.name ⇒
+                            ValDef( mods, name, obey( c )( tpt, target ), rhs )
                         case default ⇒ default
                     }
                 }
@@ -68,6 +82,24 @@ private object obeys {
         }
 
         c.Expr( ClassDef( mods, name, tparams, Template( parents, self, newBody ) ) )
+    }
+
+    def expandDef( c: whitebox.Context )( tree: c.Tree, target: c.Tree ): c.Expr[Any] = {
+        import c.universe._
+
+        val valDef = tree match {
+            case ValDef( mods, name, tpt, rhs ) ⇒
+                ValDef( mods, name, obey( c )( tpt, target ), rhs )
+            case DefDef( mods, name, tparams, vparams, tpt, rhs ) ⇒
+                DefDef( mods, name, tparams, vparams, obey( c )( tpt, target ), rhs )
+        }
+
+        c.Expr( valDef )
+    }
+
+    def obey( c: whitebox.Context )( lhs: c.Tree, rhs: c.Tree ) = {
+        import c.universe._
+        tq"_root_.io.taig.gandalf.macros.Obey[$lhs, $rhs]"
     }
 
     /**
--- macros/src/test/scala/io/taig/gandalf/macros/test/ObeysTest.scala
+++ macros/src/test/scala/io/taig/gandalf/macros/test/ObeysTest.scala
@@ -6,12 +6,47 @@ import io.taig.gandalf.macros.obeys
 import io.taig.gandalf.macros.implicits._
 
 class ObeysTest extends Suite {
-    it should "work with case class fields" in {
+    it should "support case class fields" in {
         case class User( @obeys( transformation ~> condition.success ) name:String )
         User( "foo" )
 
         case class Address( @obeys( transformation ~> condition.failure ) street:String )
         assertTypeError( """Address( "foo" )""" )
+    }
+
+    it should "support class fields" in {
+        class User( @obeys( transformation ~> condition.success ) name:String )
+        new User( "foo" )
+
+        class Address( @obeys( transformation ~> condition.failure ) street:String )
+        assertTypeError( """new Address( "foo" )""" )
+    }
+
+    it should "support val definitions" in {
+        @obeys( transformation ~> condition.success )
+        val name: String = "foo"
+
+        assertTypeError {
+            """@obeys( transformation ~> condition.failure ) val street: String = "foo""""
+        }
+    }
+
+    it should "support var definitions" in {
+        @obeys( transformation ~> condition.success )
+        var name: String = "foo"
+
+        assertTypeError {
+            """@obeys( transformation ~> condition.failure ) var street: String = "foo""""
+        }
+    }
+
+    it should "support def definitions" in {
+        @obeys( transformation ~> condition.success )
+        def name: String = "foo"
+
+        assertTypeError {
+            """@obeys( transformation ~> condition.failure ) def street: String = "foo""""
+        }
     }
 
     it should "infer the input type" in {
