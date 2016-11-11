7b4f3216a6bbf77601b646493c71884c63cbc2cc
Niklas Klein <mail@taig.io>
2016-11-11 16:13:32 +0100

    Make InferenceHelper public

--- macros/src/main/scala/io/taig/gandalf/macros/InferenceHelper.scala
+++ macros/src/main/scala/io/taig/gandalf/macros/InferenceHelper.scala
@@ -2,6 +2,6 @@ package io.taig.gandalf.macros
 
 import io.taig.gandalf.core.Rule
 
-private[gandalf] class InferenceHelper[I] {
+class InferenceHelper[I] {
     def infer[R <: Rule.Input[I]]( rule: R with Rule.Input[I] ): R = rule
 }
