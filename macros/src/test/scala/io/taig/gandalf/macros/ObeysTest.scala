950610d988e640cdd6d27194ac4d8b2d9e0acb72
Niklas Klein <mail@taig.io>
2016-12-11 22:30:00 +0100

    Improve Obey test coverage

--- macros/src/test/scala/io/taig/gandalf/macros/ObeysTest.scala
+++ macros/src/test/scala/io/taig/gandalf/macros/ObeysTest.scala
@@ -1,10 +1,33 @@
 package io.taig.gandalf.macros
 
-import io.taig.gandalf.core.Suite
+import io.taig.gandalf.core._
 import shapeless.test.illTyped
 
 class ObeysTest extends Suite {
-    it should "lift case class parameters at compile time" in {
+    it should "forward toString to the underlying value" in {
+        Obey.applyUnsafe[condition.success, String, String]( "foo" ).toString shouldBe
+            "foo"
+    }
+
+    it should "convert implicitly unwrap" in {
+        val value: String = Obey.applyUnsafe[condition.success, String, String]( "foo" )
+        value shouldBe "foo"
+    }
+
+    it should "provide unapply" in {
+        val Obey( value ) = Obey.applyUnsafe[condition.success, String, String]( "foo" )
+        value shouldBe "foo"
+    }
+
+    "lift" should "lift values at compile time" in {
+        lift( "foo" )( condition.success ).value shouldBe "foo"
+    }
+
+    it should "fail to lift values at compile time" in {
+        illTyped( """lift( "foo" )( condition.failure )""" )
+    }
+
+    "@obeys" should "lift case class parameters at compile time" in {
         ClassSuccess( "foo" ).value.value shouldBe "foo"
     }
 
